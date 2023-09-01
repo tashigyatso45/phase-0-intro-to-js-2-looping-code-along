55b5b152c98970c609fca8b17e09426d6f2355a8
Tashi Gyatso <Tashigyatso45@gmail.com>
2023-09-01 14:37:24 -0400

    for loop complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(names, events){
+const newArray=[]
+for ( let p=0; p<names.length;p++){
+newArray.push(`Thank you, ${names[p]}, for the wonderful ${events} gift!`);
+}
+return newArray
+}
+function countDown(k){
+for(let i=k; i>=0;i--){
+console.log(i)
+}
+}
